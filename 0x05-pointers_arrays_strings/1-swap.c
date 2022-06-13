293b48c3d5899348ebc7fa3c3cff8ee2c6334140
atismall <atismall128@gmail.com>
2022-06-13 14:22:30 -0700

    smiles betty certified

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*swap_int - This function swaps the number between a and b
+*@a: This is the first variable to swap
+*@b: This is the second variable to swap
+*
+*It swaps number between a and b
+*/
+
+
+void swap_int(int *a, int *b)
+{
+*a = 98;
+}
