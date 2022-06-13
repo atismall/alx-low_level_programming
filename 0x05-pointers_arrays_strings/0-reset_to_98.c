a696f7733610135c343d02a16be4951fd57baa9c
atismall <atismall128@gmail.com>
2022-06-13 14:14:16 -0700

    now it should be alright

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-void reset_to_98(int* a)
+/**
+*reset_to_98 - reset any numbers to 98
+*@a: its's my variable
+*
+*It takes a number and return 98
+*/
+
+void reset_to_98(int *a)
 {
 *a = 98;
 }
