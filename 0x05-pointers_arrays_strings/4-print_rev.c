77cbcf1837ae82e7c1e19f98aaa2807566286369
atismall <atismall128@gmail.com>
2022-06-14 10:57:59 -0700

    hopefully

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*print_rev - prints a string in reverse
+**@st: pointer to char
+*/
+
+void print_rev(char *st)
+{
+int i = 0;
+
+while (s[i])
+i++;
+
+while (i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
