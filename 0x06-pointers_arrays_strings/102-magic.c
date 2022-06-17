eb5017fcf6ba0961930611d2eace78c789dd03fb
atismall <atismall128@gmail.com>
2022-06-16 21:50:10 -0700

    good

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - another line of code
+*
+*Return: return an int
+*/
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
