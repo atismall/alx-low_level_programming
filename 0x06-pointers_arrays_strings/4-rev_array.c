f31f007a7eaab659ddf1d05192c28cc48df36b59
atismall <atismall128@gmail.com>
2022-06-16 18:29:18 -0700

    m

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,12 +13,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp;
+        int temp;
 
-	for (int i = 0; i < n / 2; i++)
-	{
-	temp = a[i];
-	a[i] = a[n - i - 1];
-	a[n - i - 1] = temp;
-	}
+        for (int i = 0; i < n / 2; i++)
+        {
+        temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+        }
 }
