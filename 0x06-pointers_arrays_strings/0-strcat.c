95f506f76ca7143ed43f138bbbcddfc839c1f688
atismall <atismall128@gmail.com>
2022-06-16 17:00:42 -0700

    already checked in vscode and with betty, you should pass o

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*_strcat - concat strings
+*@dest: variable of the destination
+*@src: variable of the source
+*
+*Return: return an int
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Hello 
+This is a read me file for the sixth projects under c programming language
