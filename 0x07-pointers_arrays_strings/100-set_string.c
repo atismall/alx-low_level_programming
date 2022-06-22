1da4e01123255c432cbe6739f8ea490f7e125b14
atismall <atismall128@gmail.com>
2022-06-21 21:29:56 -0700

    hey flat battery hurrygit add .

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * set_string - set a pointer to string *t0
+ * @s: character pointer to s
+ * @to: character pointer to to
+ *
+ * Return: Just obey my commands
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
