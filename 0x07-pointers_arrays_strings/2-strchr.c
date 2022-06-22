2c1a62933fa43ee79cf6b132a26d93196efd7ca2
atismall <atismall128@gmail.com>
2022-06-21 20:21:20 -0700

    good commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,8 @@
 
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
+	char *p = strchr(s, c);
 
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
-	{
-		if (c == s[b])
-			s += b;
-		return (s);
-	}
-	return ('\0');
+	s = p;
+	return (s);
 }
