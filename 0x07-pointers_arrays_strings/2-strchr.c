a16d12ada59880868fc3f8bea1c67f7929ab4d60
atismall <atismall128@gmail.com>
2022-06-21 20:13:26 -0700

    nice work

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - This function search for character c in string s
+ * @s: The string
+ * @c: the character to search for
+ *
+ * Return: returning string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+		return (s);
+	}
+	return ('\0');
+}
